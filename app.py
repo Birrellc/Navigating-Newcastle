4e6d978be4faef182131e6d7b69995ecc4e5cf8e
Birrellc <vaiezmedia@gmail.com>
2021-03-22 15:34:23 +0000

    Chore: moved forms from app.py to forms.py

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
+from bson.objectid import ObjectId
+from flask_pymongo import PyMongo
+from werkzeug.security import generate_password_hash, check_password_hash
 import os
 from flask import (
     Flask, flash, render_template,
     redirect, request, session, url_for)
-from flask_wtf import FlaskForm
+from forms import RegistrationForm, LoginForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Length, Email
-from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
-from werkzeug.security import generate_password_hash, check_password_hash
-from flask_pymongo import PyMongo
-from bson.objectid import ObjectId
+from flask_login import UserMixin, login_user,
+LoginManager, login_required, current_user, logout_user
 
 if os.path.exists("env.py"):
     import env
@@ -16,28 +17,41 @@ if os.path.exists("env.py"):
 
 app = Flask(__name__)
 
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
-app.secret_key = os.environ.get("SECRET_KEY")
-
 mongo = PyMongo(app)
+users = mongo.db.user
+
+login_manager = LoginManager()
+login_manager.init_app(app)
 
 
 # Forms
 
 # Code used for this class below is credited to https://pythonprogramming.net/flask-user-registration-form-tutorial/
 
-class RegistrationForm(FlaskForm):
-    username = StringField('Username',
-                           validators=[DataRequired(),
-                                       Length(min=1, max=30)])
-    email = StringField('Email',
-                        validators=[DataRequired(),
-                                    Email()])
-    password = PasswordField('Password',
-                             validators=[DataRequired(),
-                                         Length(min=8)])
-    submit = SubmitField('Sign Up')
+
+# class
+
+class User(UserMixin):
+    def __init__(self, user):
+        self.user = user
+        self.username = user['username']
+        self.id = user['_id']
+        self.email = user['email']
+        self.password = user['password']
+
+    def get_id(self):
+        object_id = self.user['_id']
+        return str(object_id)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    user = users.find_one({'_id': ObjectId(user_id)})
+    return User(user)
+
 
 # Routes
 
@@ -52,27 +66,28 @@ def home():
 
 @app.route("/signup", methods=["GET", "POST"])
 def signup():
-    signup_form = RegistrationForm()
-    return render_template('signup.html', form=signup_form)
+    form = RegistrationForm()
+    return render_template("signup.html", title="Signup", form=form)
 
 
-@app.route("/login")
+@ app.route("/login")
 def login():
-    return render_template('login.html')
+    form = LoginForm()
+    return render_template('login.html', title="Login", form=form)
 
 
-@app.route("/dictionary")
+@ app.route("/dictionary")
 def dictionary():
     dictionary = list(mongo.db.dictionary.find())
     return render_template("dictionary.html", dictionary=dictionary)
 
 
-@app.route("/profile")
+@ app.route("/profile")
 def profile():
     return render_template('profile.html')
 
 
-@app.route("/contact")
+@ app.route("/contact")
 def contact():
     return render_template('contact.html')
 
