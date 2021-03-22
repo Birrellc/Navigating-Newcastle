227f9f2bd7b95e71d550e2460d8266306c96aeb4
Birrellc <vaiezmedia@gmail.com>
2021-03-22 10:54:11 +0000

    Feat: added base of WTforms registration

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import (
     redirect, request, session, url_for)
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Length, Email
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 
@@ -22,15 +23,24 @@ mongo = PyMongo(app)
 
 # Forms
 
-class LoginForm(FlaskForm):
-    username = StringField('Username')
-    email = StringField('Email')
-    password = StringField('Password')
-    submit = SubmitField(label="Sign Up")
+# Code used for this class below is credited to https://pythonprogramming.net/flask-user-registration-form-tutorial/
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username',
+                           validators=[DataRequired(),
+                                       Length(min=1, max=30)])
+    email = StringField('Email',
+                        validators=[DataRequired(),
+                                    Email()])
+    password = PasswordField('Password',
+                             validators=[DataRequired(),
+                                         Length(min=8)])
+    submit = SubmitField('Sign Up')
 
 # Routes
 
 # Render Homepage
+
 
 @app.route("/")
 @app.route("/home")
@@ -38,9 +48,9 @@ def home():
     return render_template('index.html')
 
 
-@app.route("/signup")
+@app.route("/signup", methods=["GET", "POST"])
 def signup():
-    signup_form = LoginForm()
+    signup_form = RegistrationForm()
     return render_template('signup.html', form=signup_form)
 
 
@@ -65,7 +75,7 @@ def contact():
     return render_template('contact.html')
 
 
-if __name__ =="__main__":
+if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
-    port=int(os.environ.get("PORT")),
-    debug=True)
+            port=int(os.environ.get("PORT")),
+            debug=True)
