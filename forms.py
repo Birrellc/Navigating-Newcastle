dcf243bd2995db67d838f70fa1e146fe4803c0ee
Birrellc <vaiezmedia@gmail.com>
2021-03-26 11:20:52 +0000

    Feat: added more validation for login/signup forms

--- forms.py
+++ forms.py
@@ -1,18 +1,24 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, Length
+from wtforms.validators import DataRequired, Length, Regexp
+import re
 
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username',
-                           validators=[DataRequired(), Length(min=5, max=25)])
-    password = PasswordField('Password', validators=[DataRequired()])
+                           validators=[DataRequired(), Length(min=3, max=12,
+                            message="Must be between 3 and 12 characters long"), Regexp('^\w+$',
+                             message="Username must contain only letters numbers or underscore")])
+    password = PasswordField('Password', validators=[DataRequired(),
+                                                     Length(min=8)])
     submit = SubmitField('Sign Up')
 
 
 class LoginForm(FlaskForm):
     username = StringField('Username',
-                           validators=[DataRequired()])
+                           validators=[DataRequired(), Length(min=3, max=12,
+                            message="Must be between 3 and 12 characters long"), Regexp('^\w+$',
+                             message="Username must contain only letters numbers or underscore")])
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Login')
 
