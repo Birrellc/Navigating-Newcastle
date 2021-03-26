ff0ef02db287792346eff909a9ad55335cf07793
Birrellc <vaiezmedia@gmail.com>
2021-03-26 17:03:14 +0000

    Feat: added more validation errors

--- forms.py
+++ forms.py
@@ -2,41 +2,53 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Length, Regexp
 
+
 class RegistrationForm(FlaskForm):
     username = StringField('Username',
                            validators=[DataRequired(), Length(min=3, max=12,
-                            message="Must be between 3 and 12 characters long"), Regexp('^\w+$',
-                             message="Username must contain only letters numbers or underscore")])
+                                                              message="Please enter a username between 3 and 12 characters long"), Regexp('^\w+$',
+                                                                                                                                          message="Username must be alphanumeric and not start with a space")])
     password = PasswordField('Password', validators=[DataRequired(),
-                                                     Length(min=8)])
+                                                     Length(min=8, message="Password must be atleast 8 letters long")])
     submit = SubmitField('Sign Up')
 
 
 class LoginForm(FlaskForm):
     username = StringField('Username',
-                           validators=[DataRequired(), Length(min=3, max=12,
-                            message="Must be between 3 and 12 characters long"), Regexp('^\w+$',
-                             message="Username must contain only letters numbers or underscore")])
-    password = PasswordField('Password', validators=[DataRequired()])
+                           validators=[DataRequired(message="Please enter a username"), Length(min=3, max=12,
+                                                                                               message="Please enter a username between 3 and 12 characters long"), Regexp('^\w+$',
+                                                                                                                                                                           message="Username must be alphanumeric not start with a space")])
+    password = PasswordField('Password', [DataRequired(),
+                                          Length(min=8, message="Password must be atleast 8 letters long")])
     submit = SubmitField('Login')
 
 
 class DictionaryForm(FlaskForm):
     word = StringField('Word',
-                       validators=[DataRequired()])
-    definition = StringField('Definition', validators=[DataRequired()])
+                       validators=[DataRequired(), Regexp('^\w+$',
+                                                          message="Must be alphanumeric not start with a space")])
+    definition = StringField('Definition', validators=[DataRequired(), Regexp('^\w+$',
+                                                                              message="Must be alphanumeric not start with a space"), Length(min=2, max=25, message="must be between 2-25 characters")])
     example = StringField('Example',
-                          validators=[DataRequired()])
+                          validators=[DataRequired(), Regexp('^\w+$',
+                                                             message="Must be alphanumeric not start with a space"), Length(min=2, max=50, message="must be between 2-50 characters")])
     submit = SubmitField('Add Word')
 
 
 class UpdateWordForm(FlaskForm):
-    word = StringField('Word', validators=[DataRequired()])
-    definition = StringField('Definition', validators=[DataRequired()])
-    example = StringField('Example', validators=[DataRequired()])
+    word = StringField('Word',
+                       validators=[DataRequired(), Regexp('^\w+$',
+                                                          message="Must be alphanumeric not start with a space")])
+    definition = StringField('Definition', validators=[DataRequired(), Regexp('^\w+$',
+                                                                              message="Must be alphanumeric not start with a space"), Length(min=2, max=25, message="must be between 2-25 characters")])
+    example = StringField('Example',
+                          validators=[DataRequired(), Regexp('^\w+$',
+                                                             message="Must be alphanumeric not start with a space"), Length(min=2, max=50, message="must be between 2-50 characters")])
     submit = SubmitField('Update Word')
 
 
 class SearchForm(FlaskForm):
-    word = StringField("Search", validators=[DataRequired()])
+    word = StringField('Word',
+                       validators=[DataRequired(), Regexp('^\w+$',
+                                                          message="Must be alphanumeric not start with a space")])
     submit = SubmitField("Search Word")
