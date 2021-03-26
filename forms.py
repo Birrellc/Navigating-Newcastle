8207cf437d95d409cb1d08df7b7b2fe65816f1e5
Birrellc <vaiezmedia@gmail.com>
2021-03-26 11:07:13 +0000

    Chore: removed remail from forms.py as unused in project

--- forms.py
+++ forms.py
@@ -1,13 +1,11 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, Length, Email
+from wtforms.validators import DataRequired, Length
 
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username',
                            validators=[DataRequired(), Length(min=5, max=25)])
-    email = StringField('Email',
-                        validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Sign Up')
 
