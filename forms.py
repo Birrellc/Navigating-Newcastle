a34397c73bf35f9da1303325d458e281d0a2411b
Birrellc <vaiezmedia@gmail.com>
2021-03-31 11:59:00 +0000

    Fix: fixed collection of typos

--- forms.py
+++ forms.py
@@ -7,50 +7,50 @@ regex_key = re.compile("^[^\s].+[^\s]$")
 
 
 class RegistrationForm(FlaskForm):
-    username = StringField('Username',
+    username = StringField('Username:',
                            validators=[DataRequired(), Length(min=3, max=12,
                                                               message="Please enter a username between 3 and 12 characters long"), Regexp(regex_key,
                                                                                                                                           message="Username must be alphanumeric and not start with a space")])
-    password = PasswordField('Password', validators=[DataRequired(),
+    password = PasswordField('Password:', validators=[DataRequired(),
                                                      Length(min=8, message="Password must be atleast 8 letters long")])
     submit = SubmitField('Sign Up')
 
 
 class LoginForm(FlaskForm):
-    username = StringField('Username',
+    username = StringField('Username:',
                            validators=[DataRequired(message="Please enter a username"), Length(min=3, max=12,
                                                                                                message="Please enter a username between 3 and 12 characters long"), Regexp(regex_key,
                                                                                                                                                                            message="Username must be alphanumeric not start with a space")])
-    password = PasswordField('Password', [DataRequired(),
+    password = PasswordField('Password:', [DataRequired(),
                                           Length(min=8, message="Password must be atleast 8 letters long")])
     submit = SubmitField('Login')
 
 
 class DictionaryForm(FlaskForm):
-    word = StringField('Word',
+    word = StringField('Word:',
                        validators=[DataRequired(), Length(min=2, max=15, message="must be between 2-15 characters"), Regexp(regex_key,
                                                           message="Must be alphanumeric not start with a space")])
-    definition = StringField('Definition', validators=[DataRequired(), Regexp(regex_key,
+    definition = StringField('Definition:', validators=[DataRequired(), Regexp(regex_key,
                                                                               message="Must be alphanumeric not start with a space"), Length(min=2, max=25, message="must be between 2-25 characters")])
-    example = StringField('Example',
+    example = StringField('Example:',
                           validators=[DataRequired(), Regexp(regex_key,
                                                              message="Must be alphanumeric not start with a space"), Length(min=2, max=50, message="must be between 2-50 characters")])
-    submit = SubmitField('Add Word')
+    submit = SubmitField('Add Word:')
 
 
 class UpdateWordForm(FlaskForm):
-    word = StringField('Word',
+    word = StringField('Word:',
                        validators=[DataRequired(), Regexp(regex_key,
                                                           message="Must be alphanumeric not start with a space")])
-    definition = StringField('Definition', validators=[DataRequired(), Regexp(regex_key,
+    definition = StringField('Definition:', validators=[DataRequired(), Regexp(regex_key,
                                                                               message="Must be alphanumeric not start with a space"), Length(min=2, max=25, message="must be between 2-25 characters")])
-    example = StringField('Example',
+    example = StringField('Example:',
                           validators=[DataRequired(), Regexp(regex_key,
                                                              message="Must be alphanumeric not start with a space"), Length(min=2, max=50, message="must be between 2-50 characters")])
-    submit = SubmitField('Update Word')
+    submit = SubmitField('Update Word:')
 
 
 class SearchForm(FlaskForm):
     word = StringField("", validators=[DataRequired(), Regexp(regex_key,
                                                               message="Must be alphanumeric not start with a space")])
-    submit = SubmitField("Search Word")
+    submit = SubmitField("Search Word:")
