39c9dfa16278707463ed1f1293f60ae933f3f0f4
Birrellc <vaiezmedia@gmail.com>
2021-03-31 17:12:31 +0000

    Feat: added error pages for 403,404 & 500

--- app.py
+++ app.py
@@ -193,6 +193,21 @@ def search():
                                form=form, query=query)
 
 
+@app.errorhandler(403)
+def page_forbidden(e):
+    return render_template("403.html"), 403
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template("500.html"), 500
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
