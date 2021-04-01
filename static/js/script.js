ecb01eaecef79d7d762323a3341eda49efb87088
Birrellc <vaiezmedia@gmail.com>
2021-04-01 12:11:01 +0000

    Chore: added notes to script.js

--- static/js/script.js
+++ static/js/script.js
@@ -1,10 +1,11 @@
+// Sidenav functionality - materialize documentation
 $(document).ready(function () {
   $(".sidenav").sidenav({
     edge: "right"
   });
 });
 
-
+// Collapsible - materialize documentation
 $(document).ready(function(){
   $('.collapsible').collapsible();
 });
