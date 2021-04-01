fd2f6cdc2e9cf764f5ba890fbf137bfe0f6db6d3
Birrellc <vaiezmedia@gmail.com>
2021-04-01 11:35:01 +0000

    Chore: added notes to decorators.py

--- decorators.py
+++ decorators.py
@@ -1,13 +1,14 @@
 from functools import wraps
 from flask import flash, redirect, url_for, session
 
-
+# Function used by the Logged_in_required function to check if there is a user in session data
 def user_logged_in():
     if session.get('user') is None:
         return False
     return True
 
 
+# Login Decorator function used to return user to the login page if they try access another account which is not their own
 def logged_in_required(func):
     @wraps(func)
     def route(*args, **kwargs):
